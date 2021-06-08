5494d8f94896222582df06fc3bf3cac30a8eefe0
Dmitrii <anubisfondrx@gmail.com>
2021-06-08 20:16:28 +0700

    feat: redux for note

--- client/src/redux/notes/reducer.ts
+++ client/src/redux/notes/reducer.ts
@@ -1,4 +1,17 @@
-import { CREATE_NOTES, GET_NOTES } from "./types";
+import {
+  NOTES_GET,
+  NOTES_GET_FAIL,
+  NOTES_GET_SUCCESS,
+  NOTE_CREATE,
+  NOTE_CREATE_FAIL,
+  NOTE_CREATE_SUCCESS,
+  NOTE_DELETE,
+  NOTE_DELETE_FAIL,
+  NOTE_DELETE_SUCCESS,
+  NOTE_UPDATE,
+  NOTE_UPDATE_FAIL,
+  NOTE_UPDATE_SUCCESS,
+} from "./types";
 
 enum Align {
   left = 1,
@@ -30,9 +43,36 @@ export const notesReducer: any = (
   { type, payload }: any
 ) => {
   switch (type) {
-    case CREATE_NOTES:
+    case NOTE_CREATE:
       return { ...state, ...payload };
-
+    case NOTE_UPDATE:
+      return { ...state, ...payload };
+    case NOTE_DELETE:
+      return { ...state, ...payload };
+    case NOTES_GET:
+      return { ...state, ...payload };
+    case NOTES_GET_SUCCESS:
+      return { ...state, ...payload };
+    case NOTE_CREATE_SUCCESS:
+    case NOTE_UPDATE_SUCCESS:
+    case NOTE_DELETE_SUCCESS:
+      return {
+        ...state,
+        isLoaded: false,
+        isUpdating: false,
+        isCreating: false,
+      };
+    case NOTES_GET_FAIL:
+      return {
+        ...state,
+        data: null,
+        isLoading: false,
+        isLoaded: false,
+      };
+    case NOTE_CREATE_FAIL:
+    case NOTE_UPDATE_FAIL:
+    case NOTE_DELETE_FAIL:
+      return { ...state, isUpdating: false, isCreating: false };
     default:
       return state;
   }
